68fa9ea071a233e9254e24a738b09dfaa3c84c63
Botond Szabo <112097901+botond-szabo@users.noreply.github.com>
2025-03-22 22:14:08 -0700

    Initial commit

--- packages/@necto-style-dictionary/rollup.config.js
+++ packages/@necto-style-dictionary/rollup.config.js
@@ -1,26 +1,42 @@
 import packageJson from './package.json' with { type: 'json' };
 
 import json from '@rollup/plugin-json';
+import { dts } from "rollup-plugin-dts";
 import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import { apiExtractor } from "rollup-plugin-api-extractor";
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.min.js',
-    format: 'esm',
-    sourcemap: false,
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.min.js',
+      format: 'esm',
+      sourcemap: false,
+    },
+    external: ['style-dictionary', '@necto/color'],
+    plugins: [
+      replace({
+        preventAssignment: true,
+        __PACKAGE_NAME__: () => JSON.stringify(packageJson.name),
+      }),
+      json(),
+      terser(),
+      resolve(),
+      typescript(),
+      apiExtractor({
+        configFile: "./api-extractor.json",
+      }),
+    ]
   },
-  external: ['style-dictionary', '@necto/color'],
-  plugins: [
-    replace({
-      __PACKAGE_NAME__: () => JSON.stringify(packageJson.name),
-    }),
-    json(),
-    terser(),
-    resolve(),
-    typescript()
-  ]
-};
+  {
+    input: 'dist/index.d.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+]
