0fe45086133da97cd6b640c4537fab3678227cd7
Botond Szabo <112097901+botond-szabo@users.noreply.github.com>
2025-03-23 21:51:00 -0700

    new commit

--- packages/@necto-style-dictionary/src/transformers/dimensionToRem.ts
+++ packages/@necto-style-dictionary/src/transformers/dimensionToRem.ts
@@ -1,29 +1,41 @@
 import { isDimension } from "../filters/isDimension";
 import { getTokenValue } from "../utilities/getTokenValue";
 
-import type { PlatformConfig, Transform, TransformedToken } from 'style-dictionary/types';
+import type { Config, PlatformConfig, Transform, TransformedToken } from 'style-dictionary/types';
+
+const getBasePxFontSize = (options?: PlatformConfig): number => (options && options.basePxFontSize) || 16
 
 export const dimensionToRem: Transform = {
   name: 'dimension/rem',
-  type: `value`,
+  type: 'value',
   transitive: true,
-  filter: (token: TransformedToken) => {
-    const tokenValue = getTokenValue(token)
-    return isDimension(token) && tokenValue.substring(tokenValue.length - 2) === 'px'
-  },
-  transform: (token: TransformedToken, platform: PlatformConfig | undefined) => {
-    const tokenValue = getTokenValue(token)
-    const baseFont = platform?.basePxFontSize || 16
-    const floatVal = parseFloat(tokenValue)
+  filter: isDimension,
+  transform: (token: TransformedToken, config: PlatformConfig, options: Config) => {
+    const basePxFontSize = getBasePxFontSize(config)
+    const hasUnit = (value: string | number, unit: string): boolean => {
+      if (typeof value === 'number') {
+        return false;
+      }
+
+      return String(value).indexOf(unit) > -1;
+    };
+    const valueProp = options.usesDtcg ? '$value' : 'value';
+    const floatVal = parseFloat(token[valueProp]);
 
     if (isNaN(floatVal)) {
-      throw `[__PACKAGE_NAME__]: Invalid Number: '${token.name}: ${tokenValue}' is not a valid number, cannot transform to rem \n`
+      throw new Error(
+        `[__PACKAGE__NAME]: ]Invalid dimension token: '${token.name}: ${token[valueProp]}' is not valid and cannot be transformed to 'rem' \n`,
+      );
     }
 
     if (floatVal === 0) {
-      return '0'
+      return '0';
     }
 
-    return `${floatVal / baseFont}rem`
+    if (hasUnit(token[valueProp], 'rem') || hasUnit(token[valueProp], 'em')) {
+      return token[valueProp];
+    }
+
+    return `${floatVal / basePxFontSize}rem`;
   },
-}
+};
