ea7e7a485d8891578d299c9dfdf9e2ae20bb3c33
Botond Szabo <112097901+botond-szabo@users.noreply.github.com>
2025-06-11 17:08:00 -0700

    Update useHover.ts

--- packages/@necto-react/necto-react-hooks/src/useHover/useHover.ts
+++ packages/@necto-react/necto-react-hooks/src/useHover/useHover.ts
@@ -1,11 +1,18 @@
 // biome-ignore-all lint/suspicious/noExplicitAny: Any is fine here as some stuff will not work without it.
 
 /**
- * Copyright (c) Corinvo, LLC. and affiliates.
+ * Portions of this file are based on code from the React Aria Spectrum library by Adobe,
+ * licensed under the Apache License, Version 2.0.
+ * Copyright (c) Adobe. All rights reserved.
+ * See: https://github.com/adobe/react-spectrum
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * Modifications copyright (c) Corinvo, LLC. and affiliates. All rights reserved.
  *
+ * This file contains code licensed under:
+ * - The MIT License (see LICENSE in the root directory) for Corinvo modifications.
+ * - The Apache License, Version 2.0 for portions from Adobe.
+ *
+ * Modifications have been made to adapt the code for use in this project.
  */
 
 import { isTest } from 'std-env';
