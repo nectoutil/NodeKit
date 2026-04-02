595c9b66b4502297b48b79cbb87680541b71d29f
Botond Szabo <112097901+botond-szabo@users.noreply.github.com>
2026-04-02 11:40:52 -0700

    feat: added new hook internal id system

--- packages/@necto-react/necto-react-hooks/src/useStyleInjection/useStyleInjection.types.ts
+++ packages/@necto-react/necto-react-hooks/src/useStyleInjection/useStyleInjection.types.ts
@@ -10,11 +10,8 @@
  * Options for the useStyleInjection hook.
  */
 export interface UseStyleInjectionOptions {
-  /** Internal necto tracking ID. Used as the `necto-style-id` attribute. */
+  /** HTML `id` attribute on the `<style>` element. */
   id?: string;
-
-  /** User-facing HTML `id` attribute on the `<style>` element. */
-  elementId?: string;
 
   css: string | string[];
   window?: Window | null;
