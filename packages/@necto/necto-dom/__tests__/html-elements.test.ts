19fecc2d58fcdb8e58035f2f2b31a9e455520505
botond-szabo <112097901+botond-szabo@users.noreply.github.com>
2026-04-20 02:39:54 +0000

    chore: add license headers

--- packages/@necto/necto-dom/__tests__/html-elements.test.ts
+++ packages/@necto/necto-dom/__tests__/html-elements.test.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Corinvo, LLC. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { describe, it, expect } from 'vitest';
 import { HTMLElements } from '../src/html-elements';
 
